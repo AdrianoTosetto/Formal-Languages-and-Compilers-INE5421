e1c4fa1450be5efc663514a161eac991644efe0a
AdrianoTosetto <adriano.rafael10@hotmail.com>
2018-05-01 04:51:40 -0300

    Começando transformação de grs em automatos

--- T1/models.py
+++ T1/models.py
@@ -1,10 +1,11 @@
 class Automaton:
 
-	def __init__(self, states, finalStates, initialState):
+	def __init__(self, states, finalStates, initialState, Σ=['0','1']):
 		self.states = (states)
 		self.finalStates = (finalStates)
 		self.initialState = initialState
 		self.currentState = initialState
+		self.Σ = Σ
 
 	def process_input(self, input):
 		for symbol in input:
@@ -36,7 +37,7 @@ class NDTransition:
 		return self.target_states
 
 	def __str__(self):
-		ret = "["
+		ret = self.symbol + " -> ["
 		ret = ret + ",".join(s.__str__() for s in self.target_states)
 		ret = ret + "]"
 		return ret
@@ -101,7 +102,11 @@ class State:
 		self.transitions = []
 
 	def __str__(self):
+
 		return self.name
+
+	def __repr__(self):
+		return self.__str__()
 
 	def next_state(self, symbol):
 		for t in self.transitions:
@@ -159,8 +164,71 @@ class Grammar:
 			stringerson = stringerson + "\n"
 			first = True
 		return stringerson
+	def get_non_terminals(self):
+		ret = []
+		for p in self.productions:
+			ret.append(p.leftSide)
 
+		return list(set(ret))
+	def get_productions_from(self, non_terminal):
+		ret = []
+		for p in self.productions:
+			if p.leftSide == non_terminal:
+				ret.append(p.rightSide)
+		return ret
+	def get_ord_productions_from(self, non_terminal):
+		ret = []
+		for p in self.productions:
+			if p.leftSide == non_terminal:
+				ret.append(p.rightSide)
+		return sorted(ret, key=str.lower)
 
+	def _get_ord_productions_from(self, non_terminal):
+		sorted = self.get_ord_productions_from(non_terminal)
+		print(sorted)
+		lastSymbol = sorted[0][0]
+		add = []
+		ret = []
+		finalAdd = None
+		i = 0
+		ii = 0
+		t = 0
+		for p in sorted:
+			if p[0] == lastSymbol:
+				add.append(p)
+			else:
+				#print(add)
+				ret.append(add)
+				lastSymbol = p[0]
+				add = []
+				add.append(p)
+		ret.append(add)
+		print(ret)
+		return ret
+	def convert_to_automaton(self):
+		states = {s:NDState(s) for s in self.get_non_terminals()}
+		# state that accepts the input
+		λ = NDState('λ')
+		for s in states:
+			prods = self._get_ord_productions_from(s.__str__())
+			break
+			'''for p in prods:
+				symbol = p[0]
+				t = None
+				if len(p) == 1:
+					sset.append(λ)
+				else:
+					nt = p[1]
+					next_state = states[nt]
+					sset.append(next_state)
+				#print(s + " goes to " + str(sset) + " for " + symbol)
+				#t = NDTransition(symbol, sset)
+				#states[s].add_transition(t)'''
+		'''tst = states.values()
+		print(tst)
+		for s in tst:
+			for t in s.ndtransitions:
+				print(t)'''
 
 class Production:
 	def __init__(self, leftSide, rightSide):
@@ -172,17 +240,16 @@ class Production:
 
 	def __repr__(self):
 		return str(self)
+	def isTerminalProduction(self):
+		return len(self.rightSide) == 1
 
 '''class SententialForm:
 	def __init__(self, symbols):
 		self.symbols = symbols
-
 	def __str__(self):
 		return self.symbols
-
 	def __add__(self, form):
 		return SententialForm(self.symbols + form.symbols)
-
 	def __sub__(self, index):
 		return SententialForm("".join(self.symbols.rsplit(self.symbols[index])))'''
 
@@ -220,7 +287,7 @@ if __name__ == "__main__":
 				   Production(leftSides[1], rightSides[3]), Production(leftSides[1], rightSides[4]), Production(leftSides[1], rightSides[5]),
 				   Production(leftSides[2], rightSides[6]), Production(leftSides[2], rightSides[7])]
 	myGrammar = Grammar(productions)
-	print(myGrammar)
+	#print(myGrammar)
 	leftSides1 = ['S', 'A', 'B', 'C']
 	rightSides1 = ['aA', 'bB', 'aS', 'bC', 'b', 'bS', 'aC', 'a', 'aB', 'bA']
 	productions1 = [Production(leftSides1[0], rightSides1[0]), Production(leftSides1[0], rightSides1[1]),
@@ -228,5 +295,6 @@ if __name__ == "__main__":
 				   Production(leftSides1[2], rightSides1[5]), Production(leftSides1[2], rightSides1[6]), Production(leftSides1[2], rightSides1[7]),
 				   Production(leftSides1[3], rightSides1[8]), Production(leftSides1[3], rightSides1[9])]
 	myGrammar1 = Grammar(productions1)
+
 	print(myGrammar1)
-	myGrammar1.produce(4)
+	myGrammar1.convert_to_automaton()
