c0600ac4e00a03493679927960ecbe0ae283470c
AdrianoTosetto <adriano.rafael10@hotmail.com>
2018-06-03 16:43:51 -0300

    esqueleto da interface pronto

--- T1/view/simone_tab.py
+++ T1/view/simone_tab.py
@@ -18,20 +18,36 @@ class SimoneTab(QWidget):
 	def __init__(self):
 		super(QWidget,self).__init__()
 		self.regex_edit = QLineEdit() # re
+		self.transition_table_composition = QTableWidget()
 		self.convert_button = QPushButton("Converter")
 		self.top_layout = QGridLayout()
 		self.top_panel = QWidget()
 		self.build_top_panel()
-		self.set_click_events()
+		self.main_layout = QGridLayout()
+		self.bottom_layout = QGridLayout()
+		self.bottom_panel = QWidget()
+		self.set_bottom_panel()
 
+
+		self.set_bottom_panel()
+		self.set_main_layout()
+	def set_main_layout(self):
+		self.main_layout.addWidget(self.top_panel)
+		self.main_layout.addWidget(self.bottom_panel)
+		self.setLayout(self.main_layout)
 	def build_top_panel(self):
 		self.set_top_layout()
-		self.setLayout(self.top_layout)
+		self.top_panel.setLayout(self.top_layout)
 	def set_top_layout(self):
 		self.top_layout.addWidget(self.regex_edit)
 		self.top_layout.addWidget(self.convert_button)
 	def set_click_events(self):
 		self.convert_button.clicked.connect(self.parse_to_automaton)
 
+	def set_bottom_panel(self):
+		self.set_bottom_layout()
+		self.bottom_panel.setLayout(self.bottom_layout)
+	def set_bottom_layout(self):
+		self.bottom_layout.addWidget(self.transition_table_composition)
 	def parse_to_automaton(self):
 		print("WHAT")
