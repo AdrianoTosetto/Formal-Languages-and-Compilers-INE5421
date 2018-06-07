d56cedbff12f56e49c7a4f9d0835fe8a70fc8c4d
Giulio <cd_giulio@hotmail.com>
2018-06-07 17:44:18 -0300

    melhor ainda

--- T1/view/simone_tab.py
+++ T1/view/simone_tab.py
@@ -64,21 +64,9 @@ class SimoneTab(QWidget):
 				new_ers.append(e)
 			else:
 				if new_er in Globals.expressions:
-					alphanum = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',\
-								'p','q','r','s','t','u','v','w','x','y','z','0','1','2','3',\
-								'4','5','6','7','8','9']
-					entered = False
-					expn = ""
-					while not entered:
-						exp = expn
-						for a in alphanum:
-							exp = expn + a
-							if exp not in Globals.expressions:
-								new_er = exp
-								entered = True
-								break
-						expn += a
-				new_ers.append(new_er)
+					new_ers.append(Globals.selected)
+				else:
+					new_ers.append(new_er)
 		Globals.expressions = new_ers
 		Globals.selected = new_er
 		self.saveER.emit(Globals.selected)
