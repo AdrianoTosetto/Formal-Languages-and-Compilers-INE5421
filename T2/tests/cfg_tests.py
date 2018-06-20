0975395a5ab14a950a4f8abdbc6aa86026f17b6d
AdrianoTosetto <adriano.rafael10@hotmail.com>
2018-06-20 15:04:28 -0300

    adicionando dois testes simples

--- T2/tests/cfg_tests.py
+++ T2/tests/cfg_tests.py
@@ -1,21 +1,17 @@
+import sys
+sys.path.append('../')
+
 import unittest
+from read_tests_files import ReadTestsFiles
 
 class CFGTests(unittest.TestCase):
-    self.g1 = None
-
 
     def build_grammars(self):
-        productions = [
-            Production('S', 'A B'), Production('S', 'C D'), 
-            Production('A', 'a A'), Production('A', 'a'),
-            Production('B', 'b B'), Production('B', 'b'),
-            Production('C', 'X Y'), 
-            Production('D', 'd D'), Production('D', '&'),
-            Production('X', 'x X'), Production('X', '&'),
-            Production('Y', 'y Y'), Production('Y', '&'),    
-        ]
-        self.g1 = Grammar(productions)
-    def test_epsilon(self,)
-
+        g1 = ReadTestsFiles.read_file_and_get_grammar("g1.txt")
+        
+    def test_epsilon(self):
+        g1 = ReadTestsFiles.read_file_and_get_grammar("g1.txt")
+        self.assertEqual(g1.derives_epsilon('S'), True)
+        self.assertEqual(g1.derives_epsilon('A'), False)
 if __name__ == '__main__':
     unittest.main()
---
+++ T2/tests/read_tests_files.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('../')
+from T2.context_free_grammar import*
+
+'''
+	This class can read from a file 
+'''
+
+
+class ReadTestsFiles:
+
+	def __init__(self):
+		print()
+
+
+	'''
+		this method returns a grammar object for a given file name
+	'''
+	@staticmethod
+	def read_file_and_get_grammar(fname):
+		file = open(fname)
+		return ReadTestsFiles.raw_string_to_grammar(file.read())
+
+	'''
+		helper method
+	'''
+
+	@staticmethod
+	def raw_string_to_grammar(rawstr):
+		productions = []
+		lines = rawstr.split("\n")
+
+		for t in lines:
+			temp1 = t.split("->")
+			temp2 = temp1[1].split("|")
+
+			for tt in temp2:
+				productions.append(Production(temp1[0].strip(' '), tt.strip(' ')))
+		
+
+		return Grammar(productions)
+
+
+if __name__ == "__main__":
+	g1 = ReadTestsFiles.read_file_and_get_grammar("g1.txt")
