57bf99cb2c7a0109c564fabe4cd8421cfb42a775
AdrianoTosetto <adriano.rafael10@hotmail.com>
2018-06-22 16:18:12 -0300

    adicionando mais um test

--- T2/tests/cfg_tests.py
+++ T2/tests/cfg_tests.py
@@ -11,7 +11,13 @@ class CFGTests(unittest.TestCase):
         
     def test_epsilon(self):
         g1 = ReadTestsFiles.read_file_and_get_grammar("g1.txt")
+        g2 = ReadTestsFiles.read_file_and_get_grammar("g2.txt")
         self.assertEqual(g1.derives_epsilon('S'), True)
         self.assertEqual(g1.derives_epsilon('A'), False)
+        self.assertEqual(g2.derives_epsilon('P'), True)
+        self.assertEqual(g2.derives_epsilon('B'), True)
+        self.assertEqual(g2.derives_epsilon('K'), True)
+        self.assertEqual(g2.derives_epsilon('V'), True)
+        self.assertEqual(g2.derives_epsilon('C'), True)
 if __name__ == '__main__':
     unittest.main()
