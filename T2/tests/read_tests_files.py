452fb9c1416db87ad10226867bfddd8c108067c6
AdrianoTosetto <adriano.rafael10@hotmail.com>
2018-07-02 02:20:22 -0300

    Relatório e função para ler gramáticas e salvar gramáticas de um arquivo

--- T2/tests/read_tests_files.py
+++ T2/tests/read_tests_files.py
@@ -59,11 +59,25 @@ class FileParser:
 			#print(rawgrammar_temp1[0])
 			grammars_obj.append(ReadTestsFiles.raw_string_to_grammar(rawgrammar_temp1[1], rawgrammar_temp1[0]))
 		return grammars_obj
-
+	@staticmethod
+	def save_grammars_to_file(filename, grammars):
+		open(filename, 'w').close()
+		f = open(filename, "+a")
+		for grammar in grammars:
+			str_grammar = str(grammar)
+			str_grammar = ("\nnew_grammar\n"+grammar.name+":\n" + str_grammar).strip() + '\n'
+			f.write(str_grammar)
 if __name__ == "__main__":
 	p = FileParser()
-	print(p.get_grammars_from_file("../teste_save.txt")[0])
-	print(p.get_grammars_from_file("../teste_save.txt")[1])
+	#print(p.get_grammars_from_file("../teste_save.txt")[0])
+	gs = p.get_grammars_from_file("../teste_save.txt")
+	print(gs[0])
+	print(gs[1])
+	print("=============")
+	p.save_grammars_to_file('sd.txt', gs)
+	gs = p.get_grammars_from_file("sd.txt")
+	print(gs[0])
+	print(gs[1])
 	#g6 = ReadTestsFiles.read_file_and_get_grammar("g7.txt")
 	#print(g6)
 	#g3.make_epsilon_free(set(['P','B','C','V','K']))
